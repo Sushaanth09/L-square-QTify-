9309ce51fc470d6e3420d9feb6a9b8fce862f593
unknown <you@example.com>
2024-01-28 23:31:21 +0530

    search Bar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from "./Navbar.module.css";
 import Logo from '../Logo/Logo';
+import Search from '../Search/Search';
 
 function Navbar() {
   return (
     <nav className={styles.navbar}>
-         <a href="">
+         <a href="/">
             <Logo/>
          </a>
+         <Search placeholder="Search a album of your choice" />
     </nav>
   )
 }
