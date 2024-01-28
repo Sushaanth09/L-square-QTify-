3d6c8811c066bef870f63406338d02fc2824b769
unknown <you@example.com>
2024-01-28 23:43:33 +0530

    button

---
+++ src/components/Search/Search.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import styles from './Searchbar.module.css';
+import {ReactComponent as SearchIcon} from '../../assets/Search icon.svg';
+
+
+function Search({placeholder}) {
+  const onSubmit = (e) => {
+    e.preventDefault();
+  }
+  return (
+    <form onSubmit={onSubmit} className={styles.wrapper}>
+        <input className={styles.search} placeholder={placeholder} required/>
+        <button className={styles.searchButton} type='submit'>
+             <SearchIcon />
+        </button>
+    </form>
+  )
+}
+
+export default Search;
