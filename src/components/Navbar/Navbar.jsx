34005e71c9795817342105322ae6615979f04b69
unknown <you@example.com>
2024-01-28 22:51:48 +0530

    First Check

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import styles from "./Navbar.module.css";
-import Logo from './Logo/Logo';
+import Logo from '../Logo/Logo';
 
-export default function Navbar() {
+function Navbar() {
   return (
-    <nav className={styles.Navbar}>
-        <a href='/'>
-            <Logo />
-        </a>
+    <nav className={styles.navbar}>
+         <a href="">
+            <Logo/>
+         </a>
     </nav>
   )
 }
+
+export default Navbar;
