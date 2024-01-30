ed98c047a581e4761c8cc253c4768319800b26fc
unknown <you@example.com>
2024-01-31 01:26:02 +0530

    Carousel

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 import styles from "./Section.module.css";
 import { CircularProgress } from '@mui/material';
 import Card from '../Card/Card';
+import Carousel from '../Carousel/Carousel';
 
 function Section({ title, data, type}) {
-  const [carouselToggle, setCarouselToggle] = useState(false);
+  const [carouselToggle, setCarouselToggle] = useState(true);
 
   const handleToggle = () => {
     setCarouselToggle((prevState) => !prevState);
@@ -29,7 +30,10 @@ function Section({ title, data, type}) {
                         ))}
                     </div>
                 ) : (
-                      <div>Carousel</div>
+                      <Carousel
+                      data={data}
+                      renderComponent={(data) => <Card data={data} type={type} />} 
+                      />
                 )}
               </div>
         )
